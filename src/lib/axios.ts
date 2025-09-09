3752ff758c5f2eed1063786b07004030ae0f05db
Triananda Fajar <mail.trianandafajar@gmail.com>
2025-09-09 15:14:47 +0700

    update new api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "https://56d272ec0eff.ngrok-free.app/api/v1";
+const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "https://61922923-4543-4ae3-859e-28b74ddf48b2-00-1u2a16gl0wq0x.pike.replit.dev/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
