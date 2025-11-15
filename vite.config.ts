ed57759cd42e589d40106e77d1e35e157bd90010
Triananda Fajar <mail.trianandafajar@gmail.com>
2025-11-15 13:41:16 +0700

    update host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true, 
+    host: true
+  },
   plugins: [
     react(),
     tailwindcss()
